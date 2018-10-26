00927e7df657931961568e751de39f3e753793ef
kkamburov <Kaloyan.Kamburov@experian.com>
2018-10-26 16:43:58 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Luna"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Havanese"),        
+
+    ]);
+}
+
 const App = () => {
-    return React.createElement('div', { id: 'my-id'}, 
-        React.createElement('h1', {}, 'Adopt Me!')
-    )
+    return React.createElement('div', { id: 'my-id'}, [
+        React.createElement('h1', {}, 'Adopt Me!'),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet) 
+    ]);
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
