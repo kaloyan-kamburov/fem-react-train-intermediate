d8bc15fb05a8444149ee6891582405cd0370bf1f
kkamburov <Kaloyan.Kamburov@experian.com>
2018-11-30 16:28:35 +0200

    progress some..

--- src/App.js
+++ src/App.js
@@ -8,6 +8,34 @@ const petfinder = pf({
   secret: process.env.API_SECRET
 });
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Seattle, WA" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
+  }
   render() {
     return (
       <div>
