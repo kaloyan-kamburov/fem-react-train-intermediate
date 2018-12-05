cebcd648c660b72eb3bea2dc6921ac718374216b
kkamburov <Kaloyan.Kamburov@experian.com>
2018-12-05 17:22:09 +0200

    bla

--- src/App.js
+++ src/App.js
@@ -40,8 +40,17 @@ class App extends React.Component {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="kiki" animal="dog" breed="cat" />
-        <Pet name="ki1ki" animal="dsssog" breed="ca2t" />
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return <Pet animal={pet.animal} name={pet.name} breed={breed} />;
+          })}
+        </div>
       </div>
     );
   }
