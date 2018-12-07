50856fbd1ab462a9703186b161b5d7dc21e82dab
kkamburov <Kaloyan.Kamburov@experian.com>
2018-12-07 16:24:13 +0200

    pushec

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,13 @@
 import React from "react";
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+
+class Pet extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>dsadsa</div>;
+  }
+}
 
 export default Pet;
