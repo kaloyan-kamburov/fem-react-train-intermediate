0d5a3fb5eee9a6a69e12211fbe30f2e47e94287f
kkamburov <Kaloyan.Kamburov@experian.com>
2018-12-17 17:27:40 +0200

    push

--- SearchParams.js
+++ SearchParams.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import { ANIMALS } from "petfinder-client";
 
 class SearchParams extends Component {
   state = {
     location: "Seattle, WA",
     animal: "",
     breed: ""
+  };
+  handleLocationChange = e => {
+    this.setState({
+      location: e.target.value
+    });
+  };
+  handleAnimalChange = e => {
+    this.setState({
+      animal: e.target.value
+    });
   };
   render() {
     return (
@@ -15,7 +26,22 @@ class SearchParams extends Component {
             id="location"
             value={this.state.location}
             placeholder="Location"
+            onChange={this.handleLocationChange}
           />
+          <label htmlFor="animal">Animal</label>
+          <select
+            id="animal"
+            value={this.state.animal}
+            onChange={this.handleAnimalChange}
+            onBlur={this.handleAnimalChange}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
       </div>
     );
