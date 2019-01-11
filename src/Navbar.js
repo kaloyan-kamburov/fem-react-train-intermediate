fe87a0051a63a957d99a64dc6e8f78772013ae25
kkamburov <Kaloyan.Kamburov@experian.com>
2019-01-11 17:38:55 +0200

    styled in progress

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "@reach/router";
+import styled from "@emotion/styled";
+
+const Container = styled("header")`
+  background-color: #333;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
 
 const NavBar = () => (
-  <header>
+  <Container>
     <Link to="/">Adopt me!</Link>
     <Link to="/search-params">
       <span aria-label="search" role="img">
         Search
       </span>
     </Link>
-  </header>
+  </Container>
 );
 
 export default NavBar;
