61971b2ca5d275b86c9633e56ce76662cbcf493b
kaloyan-kamburov <kamburov.kaloyan@gmail.com>
2019-01-13 19:35:24 +0200

    emotion

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,50 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styled from "@emotion/styled";
+import colors from "./colors";
+import { keyframes } from "@emotion/core";
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg)
+  }
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+const SpyGlass = styled("span")`
+  display: inline-block;
+  animation: ${props => props.frequency}s ${spin} infinite;
+`;
 
 const Container = styled("header")`
-  background-color: #333;
+  background-color: ${colors.dark};
   position: sticky;
   top: 0;
   z-index: 10;
 `;
 
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+
+  span {
+    display: inline-block;
+    border: 1px solid red;
+    color: red !important;
+  }
+`;
+
 const NavBar = () => (
   <Container>
-    <Link to="/">Adopt me!</Link>
-    <Link to="/search-params">
-      <span aria-label="search" role="img">
+    <NavLink to="/">Adopt me!</NavLink>
+    <NavLink to="/search-params">
+      <SpyGlass frequency={5} aria-label="search" role="img">
         Search
-      </span>
-    </Link>
+      </SpyGlass>
+    </NavLink>
   </Container>
 );
 
